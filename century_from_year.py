e18a185b1a24e3ffed9d1e22f88bca85a083657b
Farnsworth2841 <31120799+Farnsworth2841@users.noreply.github.com>
2018-04-18 01:21:01 -0500

    Update century_from_year.py
    Removed 'prints'

--- century_from_year.py
+++ century_from_year.py
@@ -8,13 +8,13 @@ Created on Wed Apr 18 00:51:04 2018
 def century(year):
     # Finish this :)
     if year <= 100:
-        return print(1)
+        return 1
     elif year < 1000:
         if str(year)[-2:] != 0o1:
-            return print(int(str(year)[:1]) + 1)
+            return int(str(year)[:1]) + 1
     if year >= 1000:
         if int(str(year)[-2:]) == 00:
-            return print(int(str(year)[:2]))            
+            return int(str(year)[:2])            
     elif str(year)[-2:] != 0o1:
-        return print(int(str(year)[:2]) + 1)
+        return int(str(year)[:2]) + 1
 
