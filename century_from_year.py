3ccb1ede9288a7864065cbe0a8285fcc09e9cbd6
Farnsworth2841 <31120799+Farnsworth2841@users.noreply.github.com>
2018-10-29 16:43:41 -0500

    Previous failed
    Probably has issues with numbers ending in double 0's

--- century_from_year.py
+++ century_from_year.py
@@ -6,15 +6,32 @@ Created on Wed Apr 18 00:51:04 2018
 """
 
 def century(year):
-    # Finish this :)
-    if year <= 100:
-        return 1
-    elif year < 1000:
-        if str(year)[-2:] != 0o1:
-            return int(str(year)[:1]) + 1
-    if year >= 1000:
-        if int(str(year)[-2:]) == 00:
-            return int(str(year)[:2])            
-    elif str(year)[-2:] != 0o1:
-        return int(str(year)[:2]) + 1
+    ##Function that when given a year, will return the century it is in
+    #make sure that we have a number
+    numbah = int(year)
+    #year 12124
+    if numbah >= 1001:
+        test_number = 1000
+        while numbah > test_number:
+            test_number += 100
+        if numbah <= test_number:
+            century =  str(test_number)[-4:-2]
+    if numbah <=1000:
+        test_number = 0
+        while numbah > test_number:
+            test_number += 100
+        if numbah <= test_number:
+            century = str(test_number)[0]
+    #adding the specific century words
+    list_of_words = ['st Century', "nd Century", "rd Century", "th Century"]
+    if century[-1] == str(1):
+        return str(century) + list_of_words[0]
+    elif century[-1] == str(2):
+        return  str(century) + list_of_words[1]
+    elif century[-1] == str(3):
+        return  str(century) + list_of_words[2]
+    else:
+        return str(century) + list_of_words[-1]
+   
+    
 
